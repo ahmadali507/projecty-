20e00183d370e7b45ee9fef7117d5792608962cc
Nauman Chaudhry <naumanch969@gmail.com>
2023-12-24 18:23:04 +0500

    header files included

--- tutorial.js
+++
@@ -1,8 +0,0 @@
-// in js you do not need to specify the type of the data type like in cpp or c. 
-function result(the, word, sew, ot ){
-    var result = "";
-    result += the +" "+ word +" "+sew+" "+ot; 
-    return result; 
-
-}
-console.log(result("ahmad", "is" , "my", "naem"));
